e8df8397979649a73fc05083cc4064e1bc4431f7
Rajal Kumar <rajalkumar@protonmail.com>
2025-07-19 14:11:18 +1200

    feat(controller): set up GET index methods for Cars and Services

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::CarsController < ApplicationController
   def index
+    render json: Car.all
   end
 end
--- app/controllers/api/v1/services_controller.rb
+++ app/controllers/api/v1/services_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::ServicesController < ApplicationController
   def index
+    render json: Service.all
   end
 end
