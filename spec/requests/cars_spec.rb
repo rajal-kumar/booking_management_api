118f8dbdd20f21fad216e6882ffdfa1b14164189
Rajal Kumar <rajalkumar@protonmail.com>
2025-07-20 12:34:58 +1200

    feat(test): add pagination spec

--- spec/requests/cars_spec.rb
+++ spec/requests/cars_spec.rb
@@ -1,10 +1,21 @@
 require "rails_helper"
 
 RSpec.describe "Cars API", type: :request do
-  it "lists all cars" do
-    create(:car)
+  let(:user) { create(:user) }
+  let(:car)  { create(:car, user: user) }
 
-    get "/api/v1/cars"
+  before do
+    12.times { create(:car, user: user) }
+  end
+
+  it "returns paginated cars" do
+    get "/api/v1/cars?page=1&per_page=5", headers: auth_headers(user)
+    json = JSON.parse(response.body)
+    expect(json["data"].length).to eq(5)
+  end
+
+  it "lists all cars" do
+    get "/api/v1/cars", headers: auth_headers(user)
 
     expect(response).to have_http_status(:ok)
   end
