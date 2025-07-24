c00e43d1680beff709f964a7a66784af8efa2e75
Rajal Kumar <rajalkumar@protonmail.com>
2025-07-24 22:28:56 +1200

    chore(auth): disabled auth api now public

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :validatable, :jwt_authenticatable, jwt_revocation_strategy: self
+         :validatable, :jwt_authenticatable, jwt_revocation_strategy: Devise::JWT::RevocationStrategies::Null
 
   has_many :bookings
   has_many :cars
--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,23 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      # get "current_user", to: "current_user#index"
+
       resources :cars, only: [:index]
       resources :services, only: [:index]
       resources :bookings, only: [:index, :create]
+
+      # devise_for :users,
+      #   path: "",
+      #   path_names: {
+      #     sign_in: "login",
+      #     sign_out: "logout",
+      #     registration: "signup"
+      #   },
+      #   controllers: {
+      #     sessions: "api/v1/users/sessions",
+      #     registrations: "api/v1/users/registrations"
+      #   }
     end
   end
-  get 'current_user', to: 'current_user#index'
-  devise_for :users, path: '', path_names: {
-    sign_in: 'login',
-    sign_out: 'logout',
-    registration: 'signup'
-  },
-  controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations'
-  }
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
